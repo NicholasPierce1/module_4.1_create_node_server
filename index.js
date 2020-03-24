17e9268607ee2bceab28cd463b13b364a30e8fc1
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2020-03-24 09:10:26 -0500

    finsihed node script

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostName = process.env.hostName || '127.0.0.1';
+
+const port = process.env.PORT || 300;
+
+const server = http.createServer((request, response)=>{
+
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+
+    response.write("Hello,\n");
+    response.end("Welcome to Kohls");
+});
+
+server.listen(port, hostName, ()=>{
+    console.log(`server running at ${hostName} on port ${port}`);
+});
